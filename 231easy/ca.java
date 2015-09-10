ce1c013d2d8bc5df2c9776563560b6be208be723
wesHome <jwesheath@gmail.com>
2015-09-10 18:40:14 -0400

    added 231easy

---
+++ 231easy/ca.java
@@ -0,0 +1,41 @@
+public class ca {
+    public static void main(String[] args) {
+        
+        int[] currentState = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,
+                       0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                       0,0,0,0,0,0,0,0,0,0,0,0,0};
+        
+        int steps = Integer.parseInt(args[0]);
+
+        for (int step = 0; step < steps; step++) {
+            printArray(currentState);
+            currentState = getNextState(currentState);
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[i] == 1) {
+                System.out.print("X");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("\n");
+    }
+
+    public static int[] getNextState(int[] currentState) {
+        int[] nextState = new int[currentState.length];
+        for (int i = 1; i < currentState.length - 1; i++) {
+            if (currentState[i + 1] == currentState[i - 1]) {
+                nextState[i] = 0;
+            } else {
+                nextState[i] = 1;
+            }
+        }
+        return nextState;      
+    }
+}
---
+++ 231easy/readme.md
@@ -0,0 +1,3 @@
+[Cellular Automata: Rule 90](https://www.reddit.com/r/dailyprogrammer/comments/3jz8tt/20150907_challenge_213_easy_cellular_automata/)
+
+Simple challenge, but my first program ever in Java! Hopefully it's not too terrible.
